992147565e6e4562e3604befa3b1cc04889927c4
iLikeSoup <bneumaie@terpmail.umd.edu>
2014-11-09 01:45:13 -0500

    aaa

--- lab6/hadoop_apps/Bigram2.java
+++ lab6/hadoop_apps/Bigram2.java
@@ -19,112 +19,88 @@ import org.apache.hadoop.util.*;
 
 public class Bigram2 {
 
-public static class BigramCount implements Comparable<BigramCount>{
-	private String s1;
-	private String s2;
-	private int count;
-
-	public BigramCount(String s1, String s2, int count) {
-		this.s1 = s1;
-		this.s2 = s2;
-		this.count = count;
-	}
-
-	@Override
-	public int compareTo(BigramCount o) {
-		return (o.count - this.count);
-	}
-	
-	public String toString() {
-		return s1 + " " + s2 + ": " + count;
-	}
-}
-
 // public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 // public void map(LongWritable key, Text value, OutputCollector output, Reporter reporter) throws IOException {
 
 
-public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-	private IntWritable a = new IntWritable(1);
-    private Text word = new Text();
+public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+    private Text my_key = new Text();
+    private Text my_value = new Text();
 
     public void map(LongWritable key, Text value, OutputCollector output, Reporter reporter) throws IOException, FileNotFoundException {
 	String line = value.toString();
 	StringTokenizer tokenizer = new StringTokenizer(line);
-	HashMap<String, ArrayList<BigramCount>> my_map = new HashMap<String, ArrayList<BigramCount>>();
-	Set<String> allWords = new HashSet<String>();
+	ArrayList<String> inputList = new ArrayList<String>();
 
-	// populate data structures
 	while (tokenizer.hasMoreTokens()) {
-		String[] my_input = tokenizer.nextToken().trim().split("\\s");
-		if (my_input.length < 3) {
-			word.set("my_input len: " + my_input.length);
-			output.collect(word, a);
-			for (String i : my_input) {
-				word.set(i);
-				output.collect(word, a);
-			}
-		} else {
-				for (int x = 0; x < 2; x++) {
-				if (my_map.containsKey(my_input[x])) {
-					my_map.get(my_input[x]).add(new BigramCount(my_input[0], my_input[1], Integer.parseInt(my_input[2])));
-				} else {
-					ArrayList<BigramCount> tmp = new ArrayList<BigramCount>();
-					tmp.add(new BigramCount(my_input[0], my_input[1], Integer.parseInt(my_input[2])));
-					my_map.put(my_input[x], tmp);
-				}
-				allWords.add(my_input[x]);
-			}
-		}
+		inputList.add(tokenizer.nextToken());
 	}
-	
-	for (String s : allWords) {
-		word.set(s);
-		//output.collect(word, my_map.get(s));
-	}
-
+	my_key.set(inputList.get(0));
+	my_value.set(inputList.get(0) + " " + inputList.get(1) + " " + inputList.get(2));
+	output.collect(my_key, my_value);
     }
 }
 //public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
 //public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
 
 public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
-
-	private Text word = new Text();
+    private Text my_value = new Text();
 
     public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-//	while (values.hasNext()) {
-//		word.set(values.next());
-//		output.collect(word, word);
-/*
-	    	ArrayList<BigramCount> list = values.next();
-                int min = -1;
-                BigramCount minBigram = null;
-                Set<BigramCount> top5 = new TreeSet<BigramCount>();
-                
-                for (BigramCount b : list) {
-                        // If we found an element with a higher count than the lowest element in the current top 5
-                        if (b.count > min) {
-                                // If we need to replace an element with the new top 5 member
-                                if (top5.size() == 5) {
-                                        top5.remove(minBigram);
-                                }
-                                // New lowest member of the top 5
-                                min = b.count;
-                                minBigram = b;
-                                // Add to set
-                                top5.add(b);
-                        }
-                }
-                
-                String tostr = "";
-                for (BigramCount b : top5) {
-                        tostr += (b + " | ");
+	
+	String v = "";
+	while (values.hasNext()) {
+		v += values.next();
+		if (values.hasNext()) {
+			v += " ";
 		}
-		word.set(tostr);
-		output.collect(key, tostr);
-*/
-//	}
+	}
+	HashMap<String, Integer> my_map = new HashMap<String, Integer>();
+
+	/*while (values.hasNext()) {
+		String k = values.next().toString();
+		String delim[] = k.split("[\\d]+$");
+		my_map.put(delim[0] + " " + delim[1], Integer.parseInt(delim[2]));
+	}*/
+
+	//if (delimeter.length > 1) {
+		//for (int i = 0; i < delimeter.length; i += 3) {
+		//		my_map.put(delimeter[i] + " " + delimeter[i + 1], Integer.parseInt(delimeter[i + 2]));
+		//}
+
+		int min = -1;
+		HashSet<String> top5 = new HashSet<String>();
+		String minBigram = null;
+
+		for (String bigram : my_map.keySet()) {
+			// If we found an element with a higher count than the lowest element in the current top 5
+			if (my_map.get(bigram) > min) {
+				// If we need to replace an element with the new top 5 member
+				if (top5.size() == 5) {
+					top5.remove(minBigram);
+				}
+				// New lowest member of the top 5
+				min = my_map.get(bigram);
+				minBigram = bigram;
+				// Add to set
+				top5.add(bigram);
+			}
+		}
+		
+		String tostr = "";
+		for (String b : top5) {
+			tostr += (b + " ");
+		}
+		my_value.set(tostr);
+		output.collect(key, my_value);
+	//} else {
+		/*String tostr = "ERROR (len=" + String.valueOf(delimeter.length) + ")\t";
+		for (String b : delimeter) {
+			tostr += (b + " ");
+		}
+		my_value.set(tostr);
+		output.collect(key, my_value);		*/
+	//}
     }
 }
 
@@ -133,7 +109,7 @@ public static class Reduce extends MapReduceBase implements Reducer<Text, Text, 
      conf.setJobName("bigram2");
 
      conf.setOutputKeyClass(Text.class);
-     conf.setOutputValueClass(IntWritable.class);
+     conf.setOutputValueClass(Text.class);
 
      conf.setMapperClass(Map.class);
      conf.setCombinerClass(Reduce.class);
